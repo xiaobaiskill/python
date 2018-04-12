330b4e6acaaeddba4df09893d99dcd82f4421e6b
mac-jmz <1125378902@qq.com>
2018-04-13 07:50:34 +0800

    金字塔作业写到了函数中

--- day2/day2_proctice/multip.py
+++ day2/day2_proctice/multip.py
@@ -4,10 +4,11 @@
 
 
 
+
+'''
+#面向过程
 end = 9
-
 count = 1
-
 while count <= end :
     start = 1
     while start <=count:
@@ -16,3 +17,28 @@ while count <= end :
 
     count += 1
     print()
+
+'''
+
+
+# 函数式编程
+def multiplication_table(start,stop,step=1,order='asc'):
+    '''
+    :int start:  初始值
+    :int stop:   结束值
+    :int step:   步长    默认为1
+    :str order:  排序方式 默认正序
+    :return:     99乘法表
+    '''
+    count=1
+    while count <= stop:
+        w_start = start
+        while w_start<=count:
+            print(count,'*',w_start,'=',w_start * count,end='  ')
+            w_start+=1
+        print('')
+        count +=step
+
+multiplication_table(1,9,2)
+
+
