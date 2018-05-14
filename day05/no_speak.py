abad0f97deb2530a13cb7dc30f96dcc08e8d0db1
jmz <1125378902@qq.com>
2018-05-14 18:19:27 +0800

    添加no-speak

---
+++ day05/no_speak.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author Jmz
+
+
+# obj = range(10).__iter__()
+# print(obj.__next__())
+# print(obj.__next__())
+# print(obj.__next__())
+# print(obj.__next__())
+# print(obj.__next__())
