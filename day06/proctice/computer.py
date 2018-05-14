7710645e2d20d5b626a24f7d961f095c679e8c8b
mac-jmz <1125378902@qq.com>
2018-05-15 07:44:13 +0800

    作业开始

---
+++ day06/proctice/computer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+# Author Jmz
+
+
+import re
+
+
+expression='1-2*((60+2*(-3-40.0/5)*(9-2*5/3+7/3*99/4*2998+10*568/14))-(-4*3)/(16-3*2))'
+
+
+num1 = '-1+2*9/3*2+1-1+3-6'
+
+
+# 简单计算
+def simple_cal(exp):
+    re.findall('',exp)
+    pass
+
