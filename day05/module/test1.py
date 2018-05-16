15c6380856bbe364177895eb3083c2a4f45a4a47
mac-jmz <1125378902@qq.com>
2018-05-16 22:51:06 +0800

    博客

--- day05/module/test1.py
+++ day05/module/test1.py
@@ -3,7 +3,15 @@
 # Author Jmz
 
 import simple_mod
-simple_mod.str1 = 'test1'
-print(simple_mod.str2)
+# 导入simple_mod 模块，就是相当于 引入了 simple_mod 代码
+# 用法，在使用simple_mod 的变量或函数时名，需要在变量名或函数名加上 导入的模块名（例如本次的导入的模块名simple_mod）
 
-simple_mod.find()
+simple_mod.str1 = 'test1'    #重新赋值 simple_mod 中 str1 变量
+
+print(simple_mod.str2)
+# block dog
+
+simple_mod.find()            # 调用simple_mod 中的find函数
+# simple_mod:
+#     str1:test1
+#     str2:block dog
--- day05/module/test2.py
+++ day05/module/test2.py
@@ -3,10 +3,17 @@
 # Author Jmz
 
 from simple_mod import *
+# 导入 simple_mod 中的所有的代码
+# 在当前模块空间 创建simple_mod 的变量名函数名（+引入地址）
+
 
 print(str1)
+# jmz
 
-str1 = 'aaa'
+str1 = 'aaa'     # 当前模块 创建了一个变量名，以及内存地址
 str2 = 'bbb'
 
-find()
+find()        # 当前模块没有 定义函数 则调用simple_mod的函数，函数内的变量（或函数）皆使用simple_mod模块的变量（或函数）
+# simple_mod:
+#     str1:jmz
+#     str2:block dog
